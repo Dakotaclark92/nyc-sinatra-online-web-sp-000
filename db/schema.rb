ec2a871ca31170b354a65795b92a7c143a15ebf9
Dakota Clark <dakotaclark92@yahoo.com>
2020-01-29 05:27:09 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20190829071202) do
 
-  create_table "figure_titles", force: :cascade do |t|
-    t.integer "figure_id"
-    t.integer "title_id"
-  end
 
-  create_table "figures", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "landmarks", force: :cascade do |t|
-    t.string  "name"
-    t.integer "figure_id"
-    t.integer "year_completed"
-  end
-
-  create_table "titles", force: :cascade do |t|
-    t.string "name"
-  end
 
 end
